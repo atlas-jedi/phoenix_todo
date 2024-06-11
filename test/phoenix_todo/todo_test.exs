6720b1f3f34418546fcb92ee91cf4746127f5c05
André Pestana Silva <atlas.ifpa@gmail.com>
2024-06-11 12:11:33 -0300

    fix(tests): adjust tests to use inserted_at instead of created_at

--- test/phoenix_todo/todo_test.exs
+++ test/phoenix_todo/todo_test.exs
@@ -8,7 +8,13 @@ defmodule PhoenixTodo.TodoTest do
 
     import PhoenixTodo.TodoFixtures
 
-    @invalid_attrs %{description: nil, title: nil, completed_at: nil, created_at: nil, updated_at: nil}
+    @invalid_attrs %{
+      description: nil,
+      title: nil,
+      completed_at: nil,
+      inserted_at: nil,
+      updated_at: nil
+    }
 
     test "list_tasks/0 returns all tasks" do
       task = task_fixture()
@@ -21,13 +27,19 @@ defmodule PhoenixTodo.TodoTest do
     end
 
     test "create_task/1 with valid data creates a task" do
-      valid_attrs = %{description: "some description", title: "some title", completed_at: ~U[2024-06-10 14:10:00Z], created_at: ~U[2024-06-10 14:10:00Z], updated_at: ~U[2024-06-10 14:10:00Z]}
+      valid_attrs = %{
+        description: "some description",
+        title: "some title",
+        completed_at: ~U[2024-06-10 14:10:00Z],
+        inserted_at: ~U[2024-06-10 14:10:00Z],
+        updated_at: ~U[2024-06-10 14:10:00Z]
+      }
 
       assert {:ok, %Task{} = task} = Todo.create_task(valid_attrs)
       assert task.description == "some description"
       assert task.title == "some title"
       assert task.completed_at == ~U[2024-06-10 14:10:00Z]
-      assert task.created_at == ~U[2024-06-10 14:10:00Z]
+      assert task.inserted_at == ~U[2024-06-10 14:10:00Z]
       assert task.updated_at == ~U[2024-06-10 14:10:00Z]
     end
 
@@ -37,13 +49,20 @@ defmodule PhoenixTodo.TodoTest do
 
     test "update_task/2 with valid data updates the task" do
       task = task_fixture()
-      update_attrs = %{description: "some updated description", title: "some updated title", completed_at: ~U[2024-06-11 14:10:00Z], created_at: ~U[2024-06-11 14:10:00Z], updated_at: ~U[2024-06-11 14:10:00Z]}
+
+      update_attrs = %{
+        description: "some updated description",
+        title: "some updated title",
+        completed_at: ~U[2024-06-11 14:10:00Z],
+        inserted_at: ~U[2024-06-11 14:10:00Z],
+        updated_at: ~U[2024-06-11 14:10:00Z]
+      }
 
       assert {:ok, %Task{} = task} = Todo.update_task(task, update_attrs)
       assert task.description == "some updated description"
       assert task.title == "some updated title"
       assert task.completed_at == ~U[2024-06-11 14:10:00Z]
-      assert task.created_at == ~U[2024-06-11 14:10:00Z]
+      assert task.inserted_at == ~U[2024-06-11 14:10:00Z]
       assert task.updated_at == ~U[2024-06-11 14:10:00Z]
     end
 

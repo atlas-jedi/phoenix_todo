9d0b96109d16f70d0434da707f6e22cdff6fcba3
André Pestana Silva <atlas.ifpa@gmail.com>
2024-06-12 10:51:05 -0300

    test(controller): cover the search by query param

--- test/phoenix_todo_web/controllers/todo_controller_test.exs
+++ test/phoenix_todo_web/controllers/todo_controller_test.exs
@@ -8,6 +8,18 @@ defmodule PhoenixTodoWeb.TodoControllerTest do
     assert json_response(conn, 200)
   end
 
+  test "GET /api/tasks?=title=Buy milk&description=2% or whole?", %{conn: conn} do
+    conn = post(conn, "/api/tasks", todo: @valid_attrs)
+    conn = get(conn, "/api/tasks?title=Buy milk&description=2% or whole?")
+
+    task =
+      Enum.find(json_response(conn, 200)["data"], fn task ->
+        task["title"] == "Buy milk" and task["description"] == "2% or whole?"
+      end)
+
+    assert task["title"] == "Buy milk" and task["description"] == "2% or whole?"
+  end
+
   test "POST /api/tasks/", %{conn: conn} do
     conn = post(conn, "/api/tasks", todo: @valid_attrs)
     assert json_response(conn, 201)["data"]["title"] == "Buy milk"

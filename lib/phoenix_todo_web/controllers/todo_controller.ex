031f1a5a39e4ee675fca6bb237ebf61e175a0d3a
André Pestana Silva <atlas.ifpa@gmail.com>
2024-06-13 11:36:15 -0300

    feat: create PUT /api/tasks/:id endpoint

--- lib/phoenix_todo_web/controllers/todo_controller.ex
+++ lib/phoenix_todo_web/controllers/todo_controller.ex
@@ -19,4 +19,12 @@ defmodule PhoenixTodoWeb.TodoController do
       |> render(:show, todo: task)
     end
   end
+
+  def update(conn, %{"id" => id, "todo" => todo_params}) do
+    task = Todo.get_task!(id)
+
+    with {:ok, %Task{} = task} <- Todo.update_task(task, todo_params) do
+      render(conn, :show, todo: task)
+    end
+  end
 end
